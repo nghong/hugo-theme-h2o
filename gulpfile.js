3b475040e40495c2bf10f2222470bcd613b9f30d
Hong Nguyen <nghong@nghong-mac.local>
2019-07-27 15:18:14 +0700

    Updated gulp and libraries

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,51 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var merge = require('merge-stream');
+const { src, dest, watch, series } = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
 
-gulp.task('scripts', function(){
-gulp.src([
-    'src/js/index.js',
-    'src/lib/prism.js'
-     ])
+function concatScripts() {
+    return src([
+        'src/js/index.js',
+        'src/lib/prism.js'
+    ])
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('static/js'))
+    .pipe(dest('static/js'))
+}
+
+function compileSass() {
+    const scssStream = src(['src/scss/app.scss'])
+        .pipe(sass())
+        .pipe(concat('scss-files.scss'));
+
+    const cssStream = src([
+        'src/lib/normalize.css',
+        'src/lib/iconfont.css',
+        'src/lib/prism.css',
+        'src/lib/github-markdown.css'
+        ])
+        .pipe(concat('css-files.css'));
+
+    const mergedStream = merge(scssStream, cssStream)
+        .pipe(concat('main.css'))
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('static/css'));
+
+    return mergedStream;
+    
+}
+
+watch(['src/scss/*.scss', 'src/lib/*.css'], function(cb) {
+    compileSass();
 });
 
-gulp.task('sass', function(){
-var scssStream = gulp.src(['src/scss/app.scss'])
-    .pipe(sass())
-    .pipe(concat('scss-files.scss'));
-
-var cssStream = gulp.src([
-    'src/lib/normalize.css',
-    'src/lib/iconfont.css',
-    'src/lib/prism.css',
-    'src/lib/github-markdown.css'
-    ])
-    .pipe(concat('css-files.css'));
-
-var mergedStream = merge(scssStream, cssStream)
-    .pipe(concat('main.css'))
-    .pipe(cleanCSS())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('static/css'));
-
-return mergedStream;
+watch(['src/js/*.js', 'src/lib/*.js'], function(cb){
+    concatScripts();
 });
 
-gulp.task('watch', function(){
-gulp.watch(['src/scss/*.scss', 'src/lib/*.css'], ['sass']);
-gulp.watch(['src/js/*.js', 'src/lib/*.js'], ['scripts']);
-});
-
-gulp.task('default', ['scripts', 'sass', 'watch']);
+exports.default = series(concatScripts, compileSass);
